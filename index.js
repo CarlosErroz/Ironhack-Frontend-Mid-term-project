ab907226f9612072bf6cbc8d05667f7c239ebfeb
Carlos Erroz <carlos.erroz@hotmail.com>
2022-11-18 13:04:50 +0100

    Maquetación hecha

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ejemplo para la fecha
+<div id="current_date"></p>
+            <script>
+            date = new Date();
+            year = date.getFullYear();
+            month = date.getMonth() + 1;
+            day = date.getDate();
+            document.getElementById("current_date").innerHTML = day + "/" + month + "/" + year;
+            </script>
+*/
